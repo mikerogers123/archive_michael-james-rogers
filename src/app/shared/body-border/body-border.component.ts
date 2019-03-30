54df970c337ffa33fe80142fbcfa66898264ed1f
Mike Rogers <mikerogers_@hotmail.co.uk>
2019-03-30 17:27:26 +0000

    body border

---
+++ src/app/shared/body-border/body-border.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'body-border',
+  templateUrl: './body-border.component.html',
+  styleUrls: ['./body-border.component.css']
+})
+export class BodyBorderComponent {
+}

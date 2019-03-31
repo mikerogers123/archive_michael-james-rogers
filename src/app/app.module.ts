1958543b5eb140f760070ecb0cb630723a92fedb
Mike Rogers <mikerogers_@hotmail.co.uk>
2019-03-31 18:02:43 +0100

    get rid of dumping ground shared folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { IconsModule } from './icons/icons.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+import { NavBarComponent } from './navbar/navbar.component';
+import { BorderComponent } from './border/border.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BorderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

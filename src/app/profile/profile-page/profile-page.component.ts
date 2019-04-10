f07afee80ce8da4f3f512464698955e11df5e3f1
Mike Rogers <mikerogers_@hotmail.co.uk>
2019-04-10 20:38:19 +0100

    Typewriter experimentation

--- src/app/profile/profile-page/profile-page.component.ts
+++ src/app/profile/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typewriter from 'typewriter-effect/dist/core.js';
 
 @Component({
   selector: 'profile-page',
@@ -10,6 +11,18 @@ export class ProfilePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    new Typewriter(document.getElementById('typewriter'), {loop: false})
+      .typeString('<strong>Human: </strong>')
+      .typeString('Hello how are you?')
+      .pauseFor(2500)
+      .typeString('<br/>')
+      .typeString('<strong>Bot: </strong>')
+      .typeString('I am fine thanks, how are you?')
+      .pauseFor(2500)
+      .typeString('<br/>')
+      .typeString('<strong>Human: </strong> ')
+      .typeString('I am great!')
+      .pauseFor(2500)
+      .start();
   }
-
 }

df73f65cd2063c08f75918eb6ef686adf02a16fb
Mike Rogers <mikerogers_@hotmail.co.uk>
2019-04-10 13:23:36 +0100

    CV download

---
+++ src/app/navbar/cv-download-link/cv-download-link.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cv-download-link',
+  templateUrl: './cv-download-link.component.html'
+})
+export class CvDownloadLinkComponent {
+}
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -2,10 +2,12 @@ import { IconsModule } from './../icons/icons.module';
 import { NavBarComponent } from './navbar.component';
 import { NgModule } from '@angular/core';
 import { ImagesModule } from '../images/images.module';
+import { CvDownloadLinkComponent } from './cv-download-link/cv-download-link.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    CvDownloadLinkComponent
   ],
   imports: [
     IconsModule
